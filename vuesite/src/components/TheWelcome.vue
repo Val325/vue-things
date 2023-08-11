fde483870b3cd9139587b078f9d66745bfd6f956
Val325 <valera777pushkin777@gmail.com>
2023-08-11 18:59:42 +0300

    first commit

---
+++ vuesite/src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template v-slot:header>
+      <p>This is the header content</p>
+    </template>
+    <template v-slot:body>
+      <p>This is the body content</p>
+    </template>
+    <template v-slot:footer>
+      <p>This is the footer content</p>
+    </template>
+  </WelcomeItem>
+</template>
---
+++ vuesite/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+      <template slot="icon">
+        icon
+      </template>
+      <template slot="heading">
+        heading
+      </template>
+    <TheWelcome />
+  </main>
+</template>
