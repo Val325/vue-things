88d8ab45d1800d53fe994e302708df71b352cc55
Val325 <valera777pushkin777@gmail.com>
2023-08-12 16:51:07 +0300

    change and upload post

--- vuesite/src/views/HomeView.vue
+++ vuesite/src/views/HomeView.vue
@@ -2,6 +2,27 @@
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
+<script>
+export default {
+  data() {
+    return {
+      post: []
+    }
+  },
+  methods: {
+    getPosts() {
+      fetch('https://jsonplaceholder.typicode.com/posts/')
+        .then(response => response.json())
+        .then(data => this.post = data)
+    }
+  },
+  mounted() {
+    this.getPosts()
+  }
+}
+  
+</script>
+
 <template>
   <main>
     <TheWelcome />
@@ -12,5 +33,15 @@ import TheWelcome from '../components/TheWelcome.vue'
         heading
       </template>
     <TheWelcome />
+    <div v-for="p in post" :key="p.id">
+      <h2>{{ p.id }} - {{ p.title }}</h2>
+      <p>{{ p.body }}</p>
+    </div>
   </main>
 </template>
+
+<style>
+main {
+  text-align: center;
+}
+</style>
