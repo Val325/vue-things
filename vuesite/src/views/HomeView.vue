0f55b0b15738dc963be55a8b9b29a887cbf4118c
Val325 <valera777pushkin777@gmail.com>
2023-08-14 13:33:32 +0300

    update

--- vuesite/src/views/HomeView.vue
+++ vuesite/src/views/HomeView.vue
@@ -11,7 +11,7 @@ export default {
   },
   methods: {
     getPosts() {
-      fetch('https://jsonplaceholder.typicode.com/posts/')
+      fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
         .then(response => response.json())
         .then(data => this.post = data)
     }
@@ -25,17 +25,12 @@ export default {
 
 <template>
   <main>
-    <TheWelcome />
-      <template slot="icon">
-        icon
-      </template>
-      <template slot="heading">
-        heading
-      </template>
-    <TheWelcome />
     <div v-for="p in post" :key="p.id">
-      <h2>{{ p.id }} - {{ p.title }}</h2>
-      <p>{{ p.body }}</p>
+      <div class="post">
+        <h2 class="idnum">{{ p.id }}</h2>
+        <h2 class="title">{{ p.title }}</h2>
+        <p>{{ p.body }}</p>
+      </div>
     </div>
   </main>
 </template>
@@ -43,5 +38,15 @@ export default {
 <style>
 main {
   text-align: center;
+}
+.post {
+  border: 3px solid var(--color-border);
+  margin: 20px;
+  padding: 5px;
+  border-radius: 12px;
+}
+.idnum {
+  text-align: start;
+  margin:10px;
 }
 </style>
